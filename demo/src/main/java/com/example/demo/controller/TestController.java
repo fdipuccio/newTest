db60f3270cd6a0ec9f2d125a9f9cd6a8d1c54f2f
fabrizio.dipuccio <fabrizio.dipuccio@accenture.com>
2020-07-27 11:16:29 +0200

    modi

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -2,18 +2,41 @@ package com.example.demo.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.example.demo.model.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TestController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+    @Autowired
+    private BeanFactory beanFactory;
 
-    @GetMapping("/greeting")
+
+
+    @GetMapping("/testOpen")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "hello world";
+
+        RestTemplate  restTemplate =  beanFactory.getBean(RestTemplate.class);
+        Quote quote = restTemplate.getForObject(
+                "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+        return quote.toString();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 }
